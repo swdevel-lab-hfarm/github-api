411f2325895003be3b95a97a0026378f78657aa1
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-11-17 17:20:03 +0000

    refactored

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import argparse
 from github import get_events
 
-parser = argparse.ArgumentParser()
-parser.add_argument("username", 
-                    help="The username of the github user/organization")
-parser.add_argument("reponame", 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("username", 
+                        help="The username of the github user/organization")
+    parser.add_argument("reponame", 
                     help="The name of the repository")
-parser.add_argument("-v", help="Be more verbose", action="store_true")
-args = parser.parse_args()
+    parser.add_argument("-v", help="Be more verbose", action="store_true")
+    args = parser.parse_args()
+    return args
 
-ev = get_events(user, repo)
-print('Repository "{}" from user "{}" has {} events.'.format(repo, user, ev))
+if __name__ == "__main__":
+    args = parse_arguments()
+    ev = get_events(args.username, args.reponame, args.v)
+    print('Repository "{}" from user "{}" has {} events.'.format(args.reponame,
+                                                                 args.username,
+                                                                 ev))
