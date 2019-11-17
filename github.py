39ede12119a7db718c2e045f8033f5ac458593d1
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-11-17 17:02:39 +0000

    Introduced basic use of argparse

--- github.py
+++ github.py
@@ -3,9 +3,12 @@ import json
 
 github_URL = "https://api.github.com/repos/%s/%s/events"
 
-
-def get_events(user, repo):
+def get_events(user, repo, verbose=False):
     url = github_URL % (user, repo)
+    if verbose:
+        print("Trying to fetch the data from the API")
     r = requests.get(url)
+    if verbose:
+        print("Successfully fetched data")
     list_of_events = len(json.loads(r.text))
     return list_of_events
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import argparse
 from github import get_events
 
-user = 'swdevel-lab-hfarm'
-repo = 'example'
+parser = argparse.ArgumentParser()
+parser.add_argument("username", 
+                    help="The username of the github user/organization")
+parser.add_argument("reponame", 
+                    help="The name of the repository")
+parser.add_argument("-v", help="Be more verbose", action="store_true")
+args = parser.parse_args()
 
 ev = get_events(user, repo)
 print('Repository "{}" from user "{}" has {} events.'.format(repo, user, ev))
