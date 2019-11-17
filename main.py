07ad667bdccce33ed9d3b3c7a7f1b71009067c2f
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-11-17 17:40:51 +0000

    solves conflict

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import argparse
 from github import get_events
+valid_usernames = ['swdevel-lab-hfarm', 'swdevel-lab-h-farm-2019']
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("username", 
-                        help="The username of the github user/organization")
     parser.add_argument("reponame", 
-                    help="The name of the repository")
+                        help="The name of the repository")
+    parser.add_argument("-u", required=True, 
+                        help="The username of the github user/organization",
+                        choices = valid_usernames)
     parser.add_argument("-v", help="Be more verbose", action="store_true")
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     args = parse_arguments()
-    ev = get_events(args.username, args.reponame, args.v)
+    ev = get_events(args.u, args.reponame, args.v)
     print('Repository "{}" from user "{}" has {} events.'.format(args.reponame,
-                                                                 args.username,
+                                                                 args.u,
                                                                  ev))
