5d3c3e13b0027f524c98f34c28766bb8db60ccaf
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-11-18 12:43:57 +0100

    argument support with sys.argv

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from github import get_events
+import sys
 
-user = 'swdevel-lab-hfarm'
-repo = 'example'
+if len(sys.argv) == 3:
+    user = sys.argv[1]
+    repo = sys.argv[2]
+else:
+    print("Please run the program like: python3 main.py username reponame")
+    exit()
 
 ev = get_events(user, repo)
 print('Repository "{}" from user "{}" has {} events.'.format(repo, user, ev))
