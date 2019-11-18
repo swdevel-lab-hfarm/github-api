7ded9d2b01e622bb90b831706679863ab8c3f25d
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-11-18 09:51:56 +0000

    read defaults from CSV

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import argparse
+import csv
 from github import get_events
 valid_usernames = ['swdevel-lab-hfarm', 'swdevel-lab-h-farm-2019']
 
 
-def parse_arguments():
+def parse_allowed_repos(datafile="allowed_repos.csv"):
+    usernames = set()
+    repositories = set()
+    with open(datafile) as repo_data:
+        csv_reader = csv.reader(repo_data, delimiter=',')
+        next(csv_reader)
+        for row in csv_reader:
+            usernames.add(row[0])
+            repositories.add(row[1])
+    return usernames, repositories
+
+
+def parse_arguments(usernames, repositories):
     parser = argparse.ArgumentParser()
-    parser.add_argument("reponame", 
-                        help="The name of the repository")
-    parser.add_argument("-u", required=True, 
+    parser.add_argument("reponame",
+                        help="The name of the repository",
+                        choices=repositories)
+    parser.add_argument("-u", required=True,
                         help="The username of the github user/organization",
-                        choices = valid_usernames)
+                        choices=usernames)
     parser.add_argument("-v", help="Be more verbose", action="store_true")
-    args = parser.parse_args()
-    return args
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    args = parse_arguments()
+    usernames, repositories = parse_allowed_repos()
+    args = parse_arguments(usernames, repositories)
     ev = get_events(args.u, args.reponame, args.v)
     print('Repository "{}" from user "{}" has {} events.'.format(args.reponame,
                                                                  args.u,
