7e9ed5450ecbd9669e59757d32c97b2d46157949
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-12-06 15:43:12 +0100

    new tests with setUp and tearDown

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,15 @@
 import unittest
 from main import parse_allowed_repos
+import os
+
 
 class TestMain(unittest.TestCase):
+
+    def setUp(self):
+        # note: this would be better done with tempfile
+        self.temporary_file = "/tmp/emptyfile"
+        f = open(self.temporary_file, 'w')
+        f.close()
 
     def test_no_datafile(self):
         u, r = parse_allowed_repos(datafile="/tmp/nonexistentfile-wewefwwe")
@@ -9,6 +17,10 @@ class TestMain(unittest.TestCase):
         self.assertFalse(r)
 
     def test_empty_datafie(self):
-        u, r = parse_allowed_repos(datafile="/tmp/empty_datafile")
+        u, r = parse_allowed_repos(datafile=self.temporary_file)
         self.assertFalse(u)
         self.assertFalse(r)
+
+    def tearDown(self):
+        os.remove(self.temporary_file)
+
