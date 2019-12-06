6f00df03373e703b233320b965b93726369928b8
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-12-06 13:51:32 +0100

    now tests pass

--- github.py
+++ github.py
@@ -3,12 +3,16 @@ import json
 
 github_URL = "https://api.github.com/repos/%s/%s/events"
 
+
 def get_events(user, repo, verbose=False):
     url = github_URL % (user, repo)
     if verbose:
         print("Trying to fetch the data from the API")
     r = requests.get(url)
-    if verbose:
-        print("Successfully fetched data")
-    list_of_events = len(json.loads(r.text))
-    return list_of_events
+    if r.status_code == requests.codes.ok:
+        if verbose:
+            print("Successfully fetched data")
+        list_of_events = len(json.loads(r.text))
+        return list_of_events
+    print("Could not find the given repository: {}".format(url))
+    return 0
