a620b866fb7211ffe928fe281cb5d1fd3e601e62
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-12-06 15:35:49 +0100

    failing empty file test

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,8 @@ class TestMain(unittest.TestCase):
         u, r = parse_allowed_repos(datafile="/tmp/nonexistentfile-wewefwwe")
         self.assertFalse(u)
         self.assertFalse(r)
+
+    def test_empty_datafie(self):
+        u, r = parse_allowed_repos(datafile="/tmp/empty_datafile")
+        self.assertFalse(u)
+        self.assertFalse(r)
