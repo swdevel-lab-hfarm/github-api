81608fedf5d2a6cd62009a079cee9d6c8ba89e0a
swdevel-lab-hfarm <swdevel-lab-hfarm@unive.it>
2019-12-07 17:02:51 +0100

    adds failing tests

--- tests/test_github.py
+++ tests/test_github.py
@@ -15,3 +15,60 @@ class TestGithub(unittest.TestCase):
     def test_mismatched_repo(self):
         res = github.get_events("swdevel-lab-hfarm", "birthdays")
         self.assertFalse(res)
+
+
+class TestAnalyser(unittest.TestCase):
+
+    def testValid(self):
+        event_list = [
+            {'actor': {'login': 'u1'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u2'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u3'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u4'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u5'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            ]
+        oe, lm = github.analyse_events(event_list)
+        self.assertEqual(oe, 1)
+        self.assertEqual(lm, 1)
+
+    def testEmpty(self):
+        oe, lm = github.analyse_events([])
+        self.assertFalse(oe)
+        self.assertFalse(lm)
+
+    def testEdge(self):
+        event_list = [
+            {'actor': {'login': 'u1'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u1'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'u1'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            ]
+        oe, lm = github.analyse_events(event_list)
+        self.assertEqual(oe, 0.5)
+        self.assertEqual(lm, 1)
+
+    def testCorner(self):
+        event_list = [
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            {'actor': {'login': 'swdevel-lab-hfarm'},
+                'created_at': '2019-12-04T15:26:58Z'},
+            ]
+        oe, lm = github.analyse_events(event_list)
+        self.assertEqual(oe, 0)
+        self.assertEqual(lm, 1)
